da10f659a805d561fd7859742dfa45d1b6bec808
Joseph H Kennedy <me@jhkennedy.org>
2021-04-09 13:07:12 -0800

    fix strings and whitespace

--- tests/conftest2.py
+++ tests/conftest2.py
@@ -1,15 +1,16 @@
 import pytest
 
+
 def pytest_addoption(parser):
-    parser.addoption('--integration', action='store_true', default=False, dest="integration",
-                     help="enable integration tests")
+    parser.addoption('--integration', action='store_true', default=False, dest='integration',
+                     help='enable integration tests')
 
 
 def pytest_collection_modifyitems(config, items):
-    if not config.getoption("--integration"):
-        integration_skip = pytest.mark.skip(reason="Integration tests not requested; skipping.")
+    if not config.getoption('--integration'):
+        integration_skip = pytest.mark.skip(reason='Integration tests not requested; skipping.')
         for item in items:
-            if "integration" in item.keywords:
+            if 'integration' in item.keywords:
                 item.add_marker(integration_skip)
 
 
@@ -25,4 +26,4 @@ def rtc_raster_pair():
 @pytest.fixture(scope='session')
 def golden_water_map():
     return '/vsicurl/https://hyp3-testing.s3-us-west-2.amazonaws.com/' \
-                'asf-tools/water-map/ki-threshold-initial-water-map.tif'
+           'asf-tools/water-map/ki-threshold-initial-water-map.tif'
