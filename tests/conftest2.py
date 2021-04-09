e078c947f36391f5487281d2265e34c8b86b7573
Joseph H Kennedy <me@jhkennedy.org>
2021-04-09 13:03:21 -0800

    Add integration test for water_map

--- tests/conftest2.py
+++ tests/conftest2.py
@@ -12,3 +12,17 @@ def pytest_collection_modifyitems(config, items):
             if "integration" in item.keywords:
                 item.add_marker(integration_skip)
 
+
+@pytest.fixture(scope='session')
+def rtc_raster_pair():
+    primary = '/vsicurl/https://hyp3-testing.s3-us-west-2.amazonaws.com/' \
+              'asf-tools/water-map/ki-threshold-pototype-scene.tif'
+    secondary = '/vsicurl/https://hyp3-testing.s3-us-west-2.amazonaws.com/' \
+                'asf-tools/water-map/ki-threshold-pototype-scene.tif'
+    return primary, secondary
+
+
+@pytest.fixture(scope='session')
+def golden_water_map():
+    return '/vsicurl/https://hyp3-testing.s3-us-west-2.amazonaws.com/' \
+                'asf-tools/water-map/ki-threshold-initial-water-map.tif'
