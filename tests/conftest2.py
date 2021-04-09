67fa4fdaaac7c4e2a7077f4824cad030055904df
Joseph H Kennedy <me@jhkennedy.org>
2021-04-09 10:39:01 -0800

    skip integration tests by default, run with --integration

---
+++ tests/conftest2.py
@@ -0,0 +1,14 @@
+import pytest
+
+def pytest_addoption(parser):
+    parser.addoption('--integration', action='store_true', default=False, dest="integration",
+                     help="enable integration tests")
+
+
+def pytest_collection_modifyitems(config, items):
+    if not config.getoption("--integration"):
+        integration_skip = pytest.mark.skip(reason="Integration tests not requested; skipping.")
+        for item in items:
+            if "integration" in item.keywords:
+                item.add_marker(integration_skip)
+
